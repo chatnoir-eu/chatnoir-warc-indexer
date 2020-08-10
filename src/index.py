f6b6a6290afe7c9ac5b993677baad0490a642bf7
Janek Bevendorff <janek.bevendorff@uni-weimar.de>
2020-08-10 18:47:07 +0200

    Initial commit

---
+++ src/index.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+
+import lib
+
+import click
+import chardet
+import elasticsearch_dsl as edsl
+from elasticsearch_dsl import connections
+from elasticsearch.helpers import bulk
+from warcio.archiveiterator import ArchiveIterator
+
+from functools import partial
+import logging
+
+
+logger = logging.getLogger()
+
+
+@click.group()
+@click.option('-c', '--config', type=click.File('r'), help='Alternative configuration file')
+def main(config):
+    if config:
+        lib.get_config(config)
+
+
+@main.command()
+@click.argument('s3-bucket')
+@click.argument('meta-index')
+@click.argument('doc-id-prefix')
+def warc_offsets(s3_bucket, meta_index, doc_id_prefix):
+    """
+    Index offsets of WARC documents into Elasticsearch.
+    """
+
+    setup_metadata_index(meta_index)
+
+    sc = lib.get_spark_context()
+    s3 = lib.get_s3_resource()
+    (sc.parallelize(o.key for o in s3.Bucket(s3_bucket).objects.all())
+       .flatMap(partial(parse_warc, bucket=s3_bucket))
+       .map(partial(parse_record, doc_id_prefix=doc_id_prefix, discard_content=True))
+       .mapPartitions(partial(index_data, meta_index=meta_index, content_index=None))
+       .take(10))
+
+
+def with_config(func):
+    """Decorator for retaining configuration in Spark workers."""
+    def configured(*args, config, **kwargs):
+        lib._CONFIG.update(config)
+        return func(*args, **kwargs)
+    return partial(configured, config=lib.get_config())
+
+
+def setup_metadata_index(index_name):
+    lib.init_es_connection()
+    metadata = edsl.Index(index_name)
+    metadata.settings(**lib.get_config()['meta_index_settings'])
+
+    @metadata.document
+    class MetaDoc(edsl.Document):
+        warc_file = edsl.Keyword()
+        warc_offset = edsl.Long()
+        content_length = edsl.Long()
+        content_type = edsl.Keyword()
+        content_encoding = edsl.Keyword()
+        warc_type = edsl.Keyword()
+        warc_date = edsl.Date(format='date_time_no_millis')
+        warc_record_id = edsl.Keyword()
+        warc_warc_info_id = edsl.Keyword()
+        warc_concurrent_to = edsl.Keyword()
+        warc_ip_address = edsl.Ip()
+        warc_target_uri = edsl.Keyword()
+        warc_payload_digest = edsl.Keyword()
+        warc_block_digest = edsl.Keyword()
+
+    MetaDoc.init()
+
+
+@with_config
+def parse_warc(obj_name, bucket):
+    warc = lib.get_s3_resource().Object(bucket, obj_name).get()['Body']
+    iterator = ArchiveIterator(warc)
+    for record in iterator:
+        if record.rec_type != 'response':
+            continue
+        yield obj_name, iterator.offset, record
+
+
+@with_config
+def parse_record(warc_triple, doc_id_prefix, discard_content=False):
+    """
+    Parse WARC record into header dict and decoded content.
+    
+    :param warc_triple: triple of WARC filename, offset, record
+    :param doc_id_prefix: prefix for generating Webis UUIDs
+    :param discard_content: do not return WARC content (useful for metadata-only indexing)
+    :return: tuple of meta data and content
+    """
+    warc_file, warc_offset, warc_record = warc_triple
+
+    content = warc_record.content_stream().read()
+
+    # Try to detect content encoding
+    content_type = warc_record.http_headers.get_header('Content-Type', '').split(';')[0].strip()
+    encoding = None
+    if content_type.startswith('text/'):
+        http_enc = warc_record.http_headers.get_header('Content-Type').split(';')
+        if len(http_enc) > 1 and 'charset=' in http_enc[1]:
+            encoding = http_enc[1].replace('charset=', '').strip()
+        else:
+            encoding = chardet.detect(content).get('encoding')
+        if encoding is not None and not discard_content:
+            content = content.decode(encoding, errors='ignore')
+
+    meta = {
+        'warc_file': warc_file,
+        'warc_offset': warc_offset,
+        'content_length': int(warc_record.rec_headers.get_header('Content-Length')),
+        'content_type': warc_record.rec_headers.get_header('Content-Type'),
+        'content_encoding': encoding.lower() if encoding is not None else None,
+        **{h.replace('-', '_').lower(): v for h, v in warc_record.rec_headers.headers if h.startswith('WARC-')}
+    }
+
+    doc_id = meta['warc_trec_id'] if 'warc_trec_id' in meta else meta['warc_record_id']
+    return str(lib.get_webis_uuid(doc_id_prefix, doc_id)), meta, content if not discard_content else None
+
+
+@with_config
+def index_data(data_partition, meta_index, content_index):
+    lib.init_es_connection()
+
+    def index_gen():
+        for doc_id, meta, content in data_partition:
+            if meta:
+                yield {
+                    '_op_type': 'index',
+                    '_index': meta_index,
+                    '_id': doc_id,
+                    **meta
+                }
+            if content:
+                yield {
+                    '_op_type': 'index',
+                    '_index': content_index,
+                    '_id': doc_id,
+                    **content
+                }
+
+    bulk(edsl.connections.get_connection(), index_gen())
+    return []
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/lib.py
@@ -0,0 +1,90 @@
+import boto3
+from elasticsearch_dsl import connections
+from pyspark import SparkConf, SparkContext
+
+from functools import partial
+from glob import glob
+import json
+import os
+import uuid
+
+_CONFIG_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'conf'))
+_CONFIG = {}
+
+
+def load_properties_file(filename):
+    """
+    Load config properties file.
+
+    :param filename: file name (without .json or .local.json extension)
+    :return: config dict
+    """
+    properties = json.load(open(os.path.join(_CONFIG_DIR, '{}.json'.format(filename)), 'r'))
+    if os.path.isfile(os.path.join(_CONFIG_DIR, '{}.local.json'.format(filename))):
+        properties.update(json.load(open(os.path.join(_CONFIG_DIR, '{}.local.json'.format(filename)), 'r')))
+    return properties
+
+
+def get_config(config_file=None):
+    """
+    Load application configuration.
+
+    :param config_file: alternative config file
+    """
+    if not _CONFIG:
+        _CONFIG.update(load_properties_file('config'))
+
+    if config_file is not None:
+        _CONFIG.update(json.load(open(config_file, 'r')))
+
+    return _CONFIG
+
+
+def init_es_connection():
+    """
+    Initialize default Elasticsearch connection.
+    """
+    connections.configure(default={
+        **get_config()['es']
+    })
+
+
+def get_s3_resource():
+    """
+    :return: configured S3 resource
+    """
+    return boto3.resource('s3', **get_config()['s3'])
+
+
+def get_spark_context() -> SparkContext:
+    """
+    :return: new configured Spark context
+    """
+    if 'SPARK_HOME' not in os.environ:
+        raise RuntimeError('SPARK_HOME is not set!')
+
+    conf = SparkConf().setAppName('ChatNoir WARC Indexer').setAll(get_config()["spark"].items())
+    sc = SparkContext(conf=conf)
+    for py in glob(os.path.join(os.path.dirname(__file__), '*.py')):
+        sc.addPyFile(py)
+    return sc
+
+
+def hadoop_api(spark_context):
+    """
+    :param spark_context: configured Spark context object
+    :return: PySpark Hadoop API
+    """
+    return spark_context._jvm.org.apache.hadoop
+
+
+def get_webis_uuid(corpus_prefix, internal_id):
+    """
+    Calculate a Webis document UUID based on a corpus prefix and
+    an internal (not necessarily universally unique) doc ID.
+
+    :param corpus_prefix: corpus prefix (e.g., clueweb09, cc15, ...)
+    :param internal_id: internal doc ID (e.g., clueweb09-en0044-22-32198)
+    :return: Webis UUID
+    """
+    return uuid.uuid5(uuid.NAMESPACE_URL, ':'.join((corpus_prefix, internal_id)))
